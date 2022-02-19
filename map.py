f64c529c36bf7ba5bfe042af4550ba9ffc4535ba
Conor Bèhard Roberts <Conorbehard@hotmail.co.uk>
2022-02-19 22:42:22 +0000

    bounded the map to exeter uni

--- map.py
+++ map.py
@@ -1,17 +1,29 @@
 import folium
 
-#create map object
 
-lat = 50.721802
-long = -3.533620
+#create map object
+lat = 50.735805
+long = -3.533051
 location = [lat, long]
-m = folium.Map(location=location, zoom_start=15)
+
+#map that is bounded to Exeter Uni
+map = folium.Map(location = location,
+                 min_lon=-3.522554,
+                 max_lon=-3.543709,
+                 min_lat=50.729422,
+                 max_lat=50.743057,
+                 max_bounds=True,
+                 zoom_start = 15,
+                 min_zoom = 15)
 
 #tooltip
 tooltip = 'Click for more info'
 
 #create markers:
-folium.Marker(location, popup="<strong>Location One</strong>", tooltip=tooltip).add_to(m)
+folium.Marker(location,
+    popup="<strong>Exeter Uni</strong>",
+    tooltip=tooltip,
+    max_bounds=True).add_to(map)
 
 #generate map
-m.save('map.html')
+map.save('map.html')
