dd0fe9e182be5a20eab2f4fa4f72e679e10955ca
Conor Bèhard Roberts <47724661+Conor-Behard333@users.noreply.github.com>
2022-02-20 17:32:03 +0000

    locations from json added to map

--- map.py
+++ map.py
@@ -1,13 +1,26 @@
+import json
 import folium
 
+# Function which adds a location to the map
+def add_location(map, location, popup):
+    #tooltip
+    tooltip = 'Click for more info'
+    folium.Marker(location, popup, tooltip=tooltip).add_to(map)
+    return map
 
-#create map object
-lat = 50.735805
-long = -3.533051
-location = [lat, long]
+# Function to open and return a json file 
+def open_json_file(file_name):
+    file = open(file_name)
+    return json.load(file)
 
-#map that is bounded to Exeter Uni
-map = folium.Map(location = location,
+# Open and load json file containing locations 
+locations = open_json_file('latLong.json')
+
+# Map is centred at this location
+center = [50.735805, -3.533051]
+
+# Map that is bounded to Exeter Uni
+map = folium.Map(location = center,
                  min_lon=-3.520532,
                  max_lon=-3.548116,
                  min_lat=50.729748,
@@ -16,14 +29,11 @@ map = folium.Map(location = location,
                  zoom_start = 16,
                  min_zoom = 15)
 
-#tooltip
-tooltip = 'Click for more info'
+# Create markers for locations in the json file:
+for location in locations:
+    coords = [location['lat'], location['long']]
+    popup = location['locationName']
+    map = add_location(map, coords, popup)
 
-#create markers:
-folium.Marker(location,
-    popup="<strong>Exeter Uni</strong>",
-    tooltip=tooltip,
-    max_bounds=True).add_to(map)
-
-#generate map
+# Generate map
 map.save('map.html')
