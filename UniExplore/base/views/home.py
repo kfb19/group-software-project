9ac0d26c7da9370fa05542c391f93166796e3674
tpremoli <tomas@premoli.org>
2022-03-15 21:45:12 +0000

    Implemented weekly challenge system with auto renewing

--- UniExplore/base/views/home.py
+++ UniExplore/base/views/home.py
@@ -3,6 +3,10 @@ from ..forms import ChallengeForm
 from django.shortcuts import render
 from django.db.models import Q
 from django.utils import timezone
+from django.contrib import messages
+from django.forms import ValidationError
+import json
+from random import choice
 """
     Authors: Michael Hills
     Description: View for the main homepage
@@ -10,11 +14,15 @@ from django.utils import timezone
 def home(request):
     categories = Category.objects.all()
 
-    all_challenges = Challenges.objects.all()
+    unexpired_challenges = Challenges.objects.all()
 
-    if (len(all_challenges.exclude(Q(is_weekly_challenge=False))) == 0):
+    if (len(
+            unexpired_challenges
+            .exclude(Q(is_weekly_challenge=False))
+            .filter(Q(expires_on__gt=timezone.now()))
+            ) == 0):
         print("No weekly challenges! Generating...")
-        generate_weekly_challenges(all_challenges)
+        generate_weekly_challenges(request)
 
     # Get the filter from the ?q= in the URL
     q = request.GET.get('q') if request.GET.get('q') is not None else ''
@@ -23,14 +31,14 @@ def home(request):
     if request.user.is_authenticated:
         responses = Responses.objects.filter(user=request.user).values_list('challenge_id')
 
-        challenges = all_challenges.exclude(id__in=responses).filter(
+        challenges = Challenges.objects.exclude(id__in=responses).filter(
             Q(category__name__icontains=q),
             Q(expires_on__gt=timezone.now()) # This makes sure expired aren'trendered
             ).order_by('-created')
 
         
     else:
-        challenges = all_challenges.filter(
+        challenges = Challenges.objects.filter(
             Q(category__name__icontains=q), 
             Q(expires_on__gt=timezone.now()) # This makes sure expired aren'trendered
             ).order_by('-created')
@@ -43,16 +51,32 @@ def home(request):
 
     return render(request, 'base/home.html', context)
 
-def generate_weekly_challenges(challenges):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    name = models.CharField(max_length=200, null=True)
-    points = models.IntegerField()
-    description = models.TextField()
-    created = models.DateTimeField(auto_now_add=True)
-    expires_on = models.DateTimeField(default="2025-01-01")
-    is_weekly_challenge = models.BooleanField(default=False)
-    lat = models.FloatField(default=0)
-    long = models.FloatField(default=0)
-    form = ChallengeForm(
+def generate_weekly_challenges(request):
+    challenge_json = json.load(open('base/resources/default_challenges.json'))
+    location_json = json.load(open('base/resources/sample_locations.json'))
+
+    weekly_category = Category.objects.filter(Q(name="Weekly")).first()
+
+    expiry_date = timezone.now() + timezone.timedelta(days=7)
+
+    if(weekly_category == None):
+        messages.warning(request, 'ERROR: No weekly challenge category! Contact admin to create one.')
+        raise ValidationError("No weekly challenge category! Contact admin to create one.")
+
+    for i in range(5):
+        selected_challenge = choice(challenge_json)
+        selected_location = choice(location_json)
+
+        new_challenge = Challenges(
+            name = selected_location['name'],
+            category = weekly_category,
+            points = selected_challenge['points'],
+            description = (selected_challenge['name'] + " "  +selected_challenge['description']),
+            expires_on = expiry_date,
+            is_weekly_challenge = True,
+            lat = selected_location['lat'],
+            long = selected_location['long']
         )
+
+        new_challenge.save()
+    
