fdac602984e18654a82fa15240a90da86fe01894
Conor Bèhard Roberts <47724661+Conor-Behard333@users.noreply.github.com>
2022-03-22 16:06:28 +0000

    Create test_models.py

---
+++ UniExplore/base/tests/test_models.py
@@ -0,0 +1,10 @@
+from django.test import TestCase
+from ..models import *
+
+class TestModels(TestCase):
+    
+    def setUP(self):
+        self.user = User.objects.create(
+            username=""
+
+        )
