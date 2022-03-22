cee5a0318a02d34fabea8bd9b492fbc9dcf1a2fa
Kate Belson <kfb206@exeter.ac.uk>
2022-03-22 17:53:00 +0000

    Work done on reporting comments so far

--- UniExplore/base/views/comments.py
+++ UniExplore/base/views/comments.py
@@ -1,6 +1,6 @@
 
 from atexit import register
-from ..models import Responses,Comments
+from ..models import Category, Report, Responses,Comments
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect, render
 from .. forms import commentForm
@@ -50,3 +50,48 @@ def viewComments(request, pk):
     context = {'response': response,'comments': comments}
     return render(request, 'base/viewComments.html', context)
 
+"""
+    Authors: Kate Belson 
+    Description: View for users to report a comment
+"""
+def reportAComment(request, pk):
+    categories = Category.objects.all()
+
+    comment = Comments.objects.get(id=pk)
+
+    context = {'categories': categories, 'comment': comment}
+
+    if request.method == "POST":
+
+        reported = Report(user=request.user,reason = request.POST.get('reason'), comment=comment)
+        reported.save()
+        return redirect('home')
+
+
+    return render(request,'base/reportAComment.html',context)
+
+"""
+    Authors: Kate Belson 
+    Description: View for game masters to accept or delete reported comments
+"""
+def reportedComments(request):
+    reports = Report.objects.all()
+    categories = Category.objects.all()
+    context = {'reports': reports,'categories': categories}
+
+    if request.method == "POST":
+
+        try:
+  
+            obj = request.POST.get('commentID')
+            obj2 = request.POST.get('reportID')
+            Comments.objects.filter(id=obj).delete()
+            Report.objects.filter(id=obj2).delete()
+
+        except:
+            obj2 = request.POST.get('reportID')
+            Report.objects.filter(id=obj2).delete()
+
+
+
+    return render(request,'base/reportedPosts.html',context)
